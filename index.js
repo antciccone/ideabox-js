7214b182886709d3b3d776eb8515b1f618a837de
antciccone <aciccone@bu.edu>
2017-03-10 11:48:58 -0500

    working on edit

--- index.js
+++ index.js
@@ -21,14 +21,18 @@ $('.save-button').on('click', function(){
   idea.setLocalStorage()
 })
 
+$('body').on("blur", ".new-title-input", function(){
+  debugger;
+})
+
 $('.search').on('keyup', function(){
   var filter = this.value.toUpperCase();
   var search = $('.idea-container')
   for (i = 0; i < search.length; i++) {
-    debugger;
     td = search[i].getElementsByClassName('new-title-input')[0]
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+    ta = search[i].getElementsByClassName('quality')[0]
+    if (td || ta) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1 || ta.innerHTML.toUpperCase().indexOf(filter) > -1 ) {
         search[i].style.display = "";
        } else {
         search[i].style.display = "none";
@@ -89,9 +93,7 @@ function retrieveIdeas(){
 
 function displayIdeas(idea, id) {
   $('.list-container').prepend('<div class="idea-container"><p class="new-title-input" contenteditable="true">' + idea.title + '</p><i class="fa fa-times-circle-o delete fa-2x"" aria-hidden="true"></i><p class="new-body-input" contenteditable="true">' + idea.idea + '</p><div class="quality-style"><i class="fa fa-arrow-circle-o-up up fa-2x"" aria-hidden="true"></i><i class="fa fa-arrow-circle-o-down down fa-2x"" aria-hidden="true"></i><p class="quality">quality: ' + '<span class="quality-value">' + idea.quality + '</span>' + '</p></div></div>');
-  // <div class="list-item"' + 'id="' + id + '">
 }
-
 function Idea(title, idea) {
   this.title = title
   this.idea = idea
